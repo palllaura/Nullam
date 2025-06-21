67e0ff041f897a2d580595914d25b4bef6155b67
palllaura <palllaura.tln@gmail.com>
2025-06-21 18:53:45 +0300

    Frontend updates: functionality to delete events

--- src/main/java/com/rik/nullam/controller/web/WebController.java
+++ src/main/java/com/rik/nullam/controller/web/WebController.java
@@ -3,7 +3,10 @@ package com.rik.nullam.controller.web;
 import com.rik.nullam.service.EventService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
 
 
 @Controller
@@ -24,6 +27,16 @@ public class WebController {
     @GetMapping("/add")
     public String addEventPage() {
         return "add";
+    }
+
+    @GetMapping("/event/{eventId}")
+    public ModelAndView eventPage(@PathVariable("eventId") Long eventId) {
+        ModelAndView modelAndView = new ModelAndView("event");
+        modelAndView.getModelMap().addAttribute("now", LocalDateTime.now());
+        modelAndView.getModelMap().addAttribute("event", eventService.getEventSummaryById(eventId));
+        modelAndView.getModelMap()
+                .addAttribute("participants", eventService.getEventParticipantSummariesList(eventId));
+        return modelAndView;
     }
 
 }
